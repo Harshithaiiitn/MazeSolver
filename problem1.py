d630812f30dfe2b21399087379f74547377a6a2c
Siva Naga Harshitha B <Harshitha.b@ghc.local>
2020-06-23 21:26:28 +0530

    Add find_position for finding positions

--- problem1.py
+++ problem1.py
@@ -20,6 +20,21 @@ def display(board):
 def moves(board):
     input_sequence=input('Enter input sequence : ')
     print(input_sequence)
+    guard1_row,guard1_column=find_position('g1',board)
+    guard2_row,guard2_column=find_position('g2',board)
+    brnjolf_row,brnjolf_column=find_position('b',board)
+    exit_row,exit_column=find_position('e',board)
+    print('guard1 poisition :'+str(guard1_row)+","+str(guard1_column))
+    print('guard2 poisition :'+str(guard2_row)+","+str(guard2_column))
+    print('Brynjolf poisition :'+str(brnjolf_row)+","+str(brnjolf_column))
+    print('Exit position :'+str(exit_row)+","+str(exit_column)) 
+    
+def find_position(person,board):
+    for row in range(0,len(board)):
+        for col in range(0,len(board)):
+            if board[row][col]==person:
+                position_row,position_column=row,col
+    return position_row,position_column
 
 if __name__=='__main__':
 
